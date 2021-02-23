30c211835a74d9691b5e9d559287772fb2e15df5
acharayaP03 <acharyaP03@gmail.com>
2021-02-23 19:18:05 +1100

    added root query for the typedefs

--- server.js
+++ server.js
@@ -1,39 +1,20 @@
-const { ApolloServer, gql } = require("apollo-server");
+const fs = require("fs");
+const path = require("path");
+const { ApolloServer } = require("apollo-server");
 require("dotenv").config({ path: ".env" });
+
 const connectDb = require("./db");
 const User = require("./models/User");
 const Post = require("./models/Post");
-
 const db = process.env.MONGODB_URI;
 
 //connect to Mongo db
 connectDb(db);
 
-//define typeDefs,
-const typeDefs = gql`
-  type Todos {
-    task: String
-    completed: Boolean
-  }
+//Read typeDefs file from the file system
+const filePath = path.join(__dirname, "typeDefs.gql");
+const typeDefs = fs.readFileSync(filePath, "utf-8");
 
-  type Query {
-    getTodos: [Todos]
-  }
-`;
-
-//create a resolver
-// const resolvers = {
-//   Query: {
-//     getTodos: () => todos,
-//   },
-//   Mutation: {
-//     addTodo: (_, args) => {
-//       const todo = { task: args.task, completed: args.completed };
-//       todos.push(todo);
-//       return todo;
-//     },
-//   },
-// };
 //assign ApolloServer to the variable
 const server = new ApolloServer({
   typeDefs,
