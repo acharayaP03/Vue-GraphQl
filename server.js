e3945c947b2fee74082fd73173f069d4c7f8a12c
acharayaP03 <acharyaP03@gmail.com>
2021-02-23 22:26:34 +1100

    comments

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
-const fs = require("fs");
-const path = require("path");
 const { ApolloServer } = require("apollo-server");
 require("dotenv").config({ path: ".env" });
 
+/**
+ * @Create and import mongo Db
+ */
 const connectDb = require("./db");
 const User = require("./models/User");
 const Post = require("./models/Post");
+
+/**
+ * @read_credential from .env file
+ */
 const db = process.env.MONGODB_URI;
-const resolvers = require("./resolvers");
 
-//connect to Mongo db
 connectDb(db);
-
-//Read typeDefs file from the file system
+/**
+ * @import typeDefs and resolvers for Graph ql
+ */
+const fs = require("fs");
+const path = require("path");
 const filePath = path.join(__dirname, "typeDefs.gql");
 const typeDefs = fs.readFileSync(filePath, "utf-8");
+const resolvers = require("./resolvers");
 
-//assign ApolloServer to the variable
+/**
+ * @create GraphQL Apollo server
+ */
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -26,6 +35,11 @@ const server = new ApolloServer({
   },
 });
 
-server.listen().then(({ url }) => {
+/**
+ * @url listens on localhost:4000 by default.
+ * @change pass a PORT that we want to listen.
+ */
+const PORT = 5000 || process.env.PORT;
+server.listen(PORT).then(({ url }) => {
   console.log(`Server is listening on ${url}`);
 });
