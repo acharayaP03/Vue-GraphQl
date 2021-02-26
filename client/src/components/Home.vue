7f084b027048e8ba8efd13fea499ede1751cff85
acharayaP03 <acharyaP03@gmail.com>
2021-02-26 23:27:02 +1100

    set up apollo

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,12 +1,35 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <ul v-for="post in getPost">
+      <li>
+        {{ post.title }}
+        {{ post.imageUrl }}
+        {{ post.description }}
+      </li>
+    </ul>
     <v-btn color="primary">Submit</v-btn>
   </div>
 </template>
 
 <script>
+import { gql } from "apollo-boost";
+
 export default {
   name: "Home",
+  apollo: {
+    getPost: {
+      query: gql`
+        query {
+          getPost {
+            _id
+            title
+            imageUrl
+            description
+          }
+        }
+      `,
+    },
+  },
 };
 </script>
