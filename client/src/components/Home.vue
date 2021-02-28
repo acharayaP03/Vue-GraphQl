e08180389a20acebf0bfcb6a8a0d70c253b899e7
acharayaP03 <acharyaP03@gmail.com>
2021-02-28 21:43:25 +1100

    using apollo query component

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <div v-if="$apollo.loading">Loading.....</div>
-    <ul v-else v-for="post in getPost" :key="post.title">
-      <li>
-        {{ post.title }}
-        {{ post.imageUrl }}
-        {{ post.description }}
-      </li>
-    </ul>
-    <v-btn color="primary">Submit</v-btn>
+    <ApolloQuery :query="getPostQuery">
+      <template
+        slot-scope="{ result: { loading, data, error, networkStatus } }"
+      >
+        <div v-if="loading">Loading.....</div>
+        <div v-else-if="error">Error!!!! {{ error.message }}</div>
+        <div v-else>
+          <span>Network Status: {{ networkStatus }}</span>
+          <ul>
+            <li v-for="post in data.getPost" :key="post.title">
+              {{ post.title }}- {{ post.imageUrl }}-{{ post.description }}
+            </li>
+          </ul>
+        </div>
+      </template>
+    </ApolloQuery>
   </div>
 </template>
 
@@ -20,12 +27,7 @@ export default {
   name: "Home",
   data() {
     return {
-      post: [],
-    };
-  },
-  apollo: {
-    getPost: {
-      query: gql`
+      getPostQuery: gql`
         query {
           getPost {
             title
@@ -34,16 +36,7 @@ export default {
           }
         }
       `,
-      result({ data, loading }) {
-        if (!loading) {
-          this.post = data.getPost;
-        }
-      },
-      error(err) {
-        console.log("[ERROR]", err);
-        console.dir(err);
-      },
-    },
+    };
   },
 };
 </script>
