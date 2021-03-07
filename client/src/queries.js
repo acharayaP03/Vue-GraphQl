66545b9841e092bfdb642c326feb1016026f255b
acharayaP03 <acharyaP03@gmail.com>
2021-03-07 21:44:33 +1100

    runnign queries for signin and signup user mutations with gql variables

--- client/src/queries.js
+++ client/src/queries.js
@@ -25,3 +25,17 @@ export const GET_POSTS = gql`
 /**
  * @Post Mutations
  */
+export const SIGNIN_USER = glq`
+  mutation($username: String!, $password: String!){
+    signinUser(username: $username, $password:$password){
+      token
+    }
+  }
+`;
+export const SIGNUP_USER = gql`
+  mutation($username: String!, $email: String!, $password: String!) {
+    signupUser(username: $username, email: $email, password: $password) {
+      token
+    }
+  }
+`;
